29aada402f1d15b06aa28a5092eab884a26e4a5c
leorsn <leo007.rosengruen@gmail.com>
2026-09-14 12:57:21 +0200

    style(legal): load legal footer layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './preview.css';
+import './legal-footer.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
